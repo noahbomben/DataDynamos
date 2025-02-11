17365036222306366e8fdd84891c2ac324a44cbe
bombenn <bombenn@wwu.edu>
2025-02-10 21:33:29 -0800

    merging

--- remote-collaboration/src/components/App/App.jsx
+++ remote-collaboration/src/components/App/App.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import './App.css'
 import {
-  BrowserRouter as Router,
   Routes,
   Route,
 } from "react-router-dom"
-import LoginPage from "../LoginPage/LoginPage.jsx"
+import LoginPage from "../LoginPage/LoginPage"
 import SignUpPage from "../SignUpPage/SignUpPage.jsx"
 
 function App() {
   return (
-    <Router>
+
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/SignUpPage" element={<SignUpPage />} />
       </Routes>
-    </Router>
+
   )
 }
 
