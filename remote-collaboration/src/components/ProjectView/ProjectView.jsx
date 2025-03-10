b61a8530dfb5bfe1c328243c9cf859dc7dc43864
Alex Ballew <Ajb8241@gmail.com>
2025-03-10 00:09:24 -0700

    backend functionality added for texting

--- remote-collaboration/src/components/ProjectView/ProjectView.jsx
+++ remote-collaboration/src/components/ProjectView/ProjectView.jsx
@@ -3,8 +3,9 @@ import './ProjectView.css'
 import { useNavigate, useLocation } from "react-router-dom";
 import WhiteBoard from "../WhiteBoard/WhiteBoard";
 import FileUpload from "../FileUpload/FileUpload";
+import toast, { Toaster } from "react-hot-toast";
 
-function ProjectView() {
+function ProjectView() { 
     const navigate = useNavigate()
     const location = useLocation();
     const project = location.state;
@@ -14,6 +15,7 @@ function ProjectView() {
     }
     return(
         <>
+            <Toaster></Toaster>
             <div className="projectview_page">
                 <button className="close_project_button" onClick={closeProject}>Home</button>
                 <div className="projectview-container">
@@ -44,21 +46,51 @@ function ProjectView() {
 }
 
 // MOVE later
-var x = [['green', 'hello'], ['blue', 'hey, when are we meeting?'], ['yellow', 'I thought today... No?'],
-['blue', 'I think we should meet sometime next week lets take a look at the schedule']];
+// var x = [['green', 'hello'], ['blue', 'hey, when are we meeting?'], ['yellow', 'I thought today... No?'],
+// ['blue', 'I think we should meet sometime next week lets take a look at the schedule']];
 
 
 function ChatBox() {
 
-    const [fakeData, setFakeData] = useState(x);
+    const [userData, setUserData] = useState([]);
     const divRef = useRef(null);
-
-
+    const email = localStorage.getItem('email');
+    const projectID = "1"; // hardcoded for now, change later
+    
     useEffect(() => {
-        scrollToBottom();
-    }, [fakeData])
+        getMessages();
+        setTimeout(() => {
+            scrollToBottom();
+        }, 500);
+
+    }, [])
+
+    const getMessages = async () => {
+        try {
+            const queryString = new URLSearchParams({projectID}).toString();
+            const response = await fetch(`http://localhost:3000/api/messages?${queryString}`, {
+            method: "GET",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            })
+    
+            const responseData = await response.json()
+            if (response.ok) {
+                console.log(responseData);
+                setUserData(responseData.data)
+            } else {
+                console.log(responseData);
+                toast.error("An error occurred getting messages");
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error("An error occurred getting messages");
+        }
+    }
 
     const scrollToBottom = () => {
+        console.log("scrolling");
         if (divRef.current) {
             divRef.current.scrollTop = divRef.current.scrollHeight;
         } 
@@ -68,49 +100,85 @@ function ChatBox() {
         <div className="chat">
             <div className="chat-scroll"  ref={divRef}>
                 {
-                    fakeData.map((item) => {
-                        let profile = item[0];
+                    userData.map((item) => {
+                        let email = item[0];
                         let msg = item[1];
                         return(
-                            <Message profileIcon={profile} msg={msg}></Message>
+                            <Message email={email} msg={msg}></Message>
                         )
                     })
                 }
-            </div>
-            <SendText setFakeData={setFakeData} fakeData={fakeData}></SendText>
+            </div> {/* Change later to condition render in different spots depending if current user sent*/}
+            <SendText setUserData={setUserData} userData={userData} email={email} projectID={projectID} scrollToBottom={scrollToBottom}></SendText>  
         </div>
     )
 }
 
-function SendText({setFakeData, fakeData}) {
+function SendText({setUserData, userData, email, projectID, scrollToBottom}) {
 
-    const [text, setText] = useState("");
+    const [message, setMessage] = useState("");
+    const date = new Date();
 
     const updateText = (e) => {
-        setText(e.target.value);
+        setMessage(e.target.value);
     }
 
-    const sendText = () => {
-        if (text.length > 0) {
-            setFakeData([... fakeData, ['black', text]]); 
-            setText("");  
-        }  
+    const sendText = async () => {
+        const time = date.getTime();
+        const data = {email, projectID, message, time}
+        try {
+            const response = await fetch("http://localhost:3000/api/messages", {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(data)
+            })
+    
+            const responseData = await response.json()
+            if (response.ok) {
+                setUserData([... userData, [email, message]]); 
+                setMessage("");  
+                setTimeout(() => {
+                scrollToBottom();
+                    
+                }, 500);
+            } else {
+                console.log(responseData);
+                toast.error("An error occurred sending message, please try again");
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error("An error occurred sending message, please try again");
+        }
     }
 
     return (  /* img cut off fix later*/
         <div className="text-container">
-            <input className="text" placeholder="Write message" onChange={updateText} value={text}></input>
+            <input className="text" placeholder="Write message" onChange={updateText} value={message}></input>
             <button className="send-text" onClick={sendText}></button>
         </div>
     )
 }
 
-function Message({profileIcon, msg}) {
+function Message({email, msg}) {
+    const userEmail = localStorage.getItem('email');
+
     return (
-        <div className="msg-container">
-            <div className="profile-icon" style={{backgroundColor: profileIcon}}></div>
-            <div className="msg"><p style={{overflowWrap: "break-word"}}>{msg}</p></div>
-        </div>
+        <>
+            {userEmail !== email ? (
+                <div className="msg-container">
+                    <div className="profile-icon">{email[0].toUpperCase()}</div>
+                    <div className="msg"><p style={{overflowWrap: "break-word"}}>{msg}</p></div>
+                </div>
+                
+            ) : (
+                <div className="msg-container" style={{justifyContent: "end"}}>
+                    <div className="msg"><p style={{overflowWrap: "break-word"}}>{msg}</p></div>
+                    <div className="profile-icon" style={{marginLeft: "2%", marginRight: "1%"}}>{email[0].toUpperCase()}</div>
+                </div>
+            )}
+        </>
     )
 }
 
